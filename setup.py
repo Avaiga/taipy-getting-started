9e2139a4a310bf4ad6e7103bd768deee61b14832
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2022-04-15 17:28:31 +0200

    attempt

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Copyright 2022 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+"""The setup script."""
+
+from setuptools import setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Avaiga",
+    author_email="dev@taipy.io",
+    python_requires=">=3.8",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    keywords="taipy-getting-started",
+    name="taipy-getting-started",
+    url="https://github.com/avaiga/taipy-core",
+    version="1.0.0.dev",
+    zip_safe=False,
+)
